b31e4fe7dd1723c8f1ec56547bd6bcb50bc8b3df
ailton <ailton.xavier@fomento.to.gov.br>
2024-08-21 18:01:07 -0300

    update

--- app/app/(dashboard)/juridico/negociacao/_components/formParcela.tsx
+++ app/app/(dashboard)/juridico/negociacao/_components/formParcela.tsx
@@ -80,153 +80,168 @@ export default function FormParcela({
     event.preventDefault();
   }
   return (
-    <>
-      <div className="w-full flex justify-end">
+    <div className="w-full flex justify-end">
+      <Card>
+        <Dialog open={modalIsOpen} onOpenChange={setOpenChange}>
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              type="button"
+              size="sm"
+              title={id ? "Editar" : "Adicionar Parcela"}
+              onClick={() => setModalIsOpen(true)}
+            >
+              {id ? (
+                <Pencil2Icon className="h-4 w-4" />
+              ) : (
+                <PlusIcon className="h-4 w-4" />
+              )}
+            </Button>
+          </DialogTrigger>
 
-        <Card>
-          <Dialog open={modalIsOpen} onOpenChange={setOpenChange}>
-            <DialogTrigger asChild>
-              <Button
-                variant={"outline"}
-                type="button"
-                size={"sm"}
-                title={id ? "editar" : "adicionar parcela"}
-                onClick={() => setModalIsOpen(true)}
-              >
-                {id ? (
-                  <Pencil2Icon className="h-4 w-4" />
-                ) : (
-                  <PlusIcon className="h-4 w-4" />
-                )}
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Cadastro de Parcela</DialogTitle>
-                <DialogDescription></DialogDescription>
-              </DialogHeader>
-              <form onSubmit={handleModule}>
-                <div className="grid gap-4 py-2">
-                  <div className="grid grid-flow-col gap-2 justify-start items-start space-x-1">
-                    {id > 0 && (
-                      <div className="flex flex-col w-36 justify-start items-start space-y-2">
-                        <Label className="text-right">
-                          ID:
-                        </Label>
-                        <Input disabled value={parcela.id ?? ""} />
-                      </div>
-                    )}
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Cadastro de Parcela</DialogTitle>
+              <DialogDescription></DialogDescription>
+            </DialogHeader>
+
+            <form onSubmit={handleModule}>
+              <div className="grid gap-4 py-2">
+
+                {/* ID and Tipo */}
+                <div className="grid grid-flow-col gap-2 justify-start items-start space-x-1">
+                  {id > 0 && (
                     <div className="flex flex-col w-36 justify-start items-start space-y-2">
-                      <Label htmlFor="tipo-acordo">Tipo</Label>
-                      <Select value={'' + type} onValueChange={(value) => {
-                        setType(value)
+                      <Label>ID:</Label>
+                      <Input disabled value={parcela.id ?? ""} />
+                    </div>
+                  )}
+                  <div className="flex flex-col w-36 justify-start items-start space-y-2">
+                    <Label htmlFor="tipo-acordo">Tipo</Label>
+                    <Select
+                      value={'' + type}
+                      onValueChange={(value) => {
+                        setType(value);
                         setParcela({
                           ...parcela,
                           type: parseInt(value),
                         });
-                      }}>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Selecione um tipo" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="1">Contrato</SelectItem>
-                          <SelectItem value="2">Entrada</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div>
+                      }}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione um tipo" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="1">Contrato</SelectItem>
+                        <SelectItem value="2">Entrada</SelectItem>
+                      </SelectContent>
+                    </Select>
                   </div>
-                  <div className="grid grid-flow-col m-2 justify-start items-start space-x-1">
-                    <div className="flex flex-col space-y-2">
-                      <Label>Data</Label>
-                      <InputDateForm
-                        field={parcela?.data}
-                        onClickDay={handleChangeData}
-                      />
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                      <Label>Data Pagamento</Label>
-                      <InputDateForm
-                        field={parcela?.data_pgto}
-                        onClickDay={handleChangeDataPgto}
-                      />
-                    </div>
-                  </div>
-                  <div className="grid grid-flow-col gap-1 m-2 items-start justify-start">
-                    <div className="flex flex-col items-start space-y-2 w-36">
-                      <Label htmlFor='val_parcela'>Valor Parcela</Label>
-                      <Input
-                        id="val_parcela"
-                        type="number"
-                        required
-                        step="0.01"
-                        min="0"
-                        value={parcela?.val_parcela}
-                        onChange={(e) => {
-                          setParcela({
-                            ...parcela,
-                            val_parcela: e.target.value,
-                          });
-                        }}
-                      />
-                    </div>
-                    <div className="flex flex-col items-start space-y-2">
-                      <Label htmlFor='val_pago'>Primeira Parcela Acordo</Label>
-                      <Input
-                        id="val_pago"
-                        type="number"
-                        required
-                        step="0.01"
-                        min="0"
-                        value={parcela?.val_pago}
-                        onChange={(e) => {
-                          setParcela({
-                            ...parcela,
-                            val_pago: e.target.value,
-                          });
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div className="space-y-2 gap-2">
-                      <Checkbox id="jurus" />
-                      <Label htmlFor="jurus">Juros</Label>
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div className="space-y-2 gap-2">
-                      <Label htmlFor="jurus">Obs.</Label>
-                      <Textarea className="w-full" />
-                    </div>
-                  </div>
+                </div>
 
+                {/* Data and Data Pagamento */}
+                <div className="grid grid-flow-col m-2 justify-start items-start space-x-1">
+                  <div className="flex flex-col space-y-2">
+                    <Label>Data</Label>
+                    <InputDateForm
+                      field={parcela?.data}
+                      onClickDay={handleChangeData}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-2">
+                    <Label>Data Pagamento</Label>
+                    <InputDateForm
+                      field={parcela?.data_pgto}
+                      onClickDay={handleChangeDataPgto}
+                    />
+                  </div>
                 </div>
-                <div className="flex items-end justify-end gap-2">
-                  <Button
-                    type="button"
-                    variant={"secondary"}
-                    onClick={() => closeModal()}
-                  >
-                    Cancelar
-                  </Button>
-                  <Button type="submit">Salvar</Button>
+
+                {/* Valor Parcela and Primeira Parcela */}
+                <div className="grid grid-flow-col gap-1 m-2 items-start justify-start">
+                  <div className="flex flex-col items-start space-y-2 w-36">
+                    <Label htmlFor="val_parcela">Valor Parcela</Label>
+                    <Input
+                      id="val_parcela"
+                      type="number"
+                      required
+                      step="0.01"
+                      min="0"
+                      value={parcela?.val_parcela}
+                      onChange={(e) => {
+                        setParcela({
+                          ...parcela,
+                          val_parcela: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start space-y-2">
+                    <Label htmlFor="val_pago">Primeira Parcela Acordo</Label>
+                    <Input
+                      id="val_pago"
+                      type="number"
+                      required
+                      step="0.01"
+                      min="0"
+                      value={parcela?.val_pago}
+                      onChange={(e) => {
+                        setParcela({
+                          ...parcela,
+                          val_pago: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
-              </form>
-              <DialogFooter className="w-full">
-                <section className="w-full">
-                  {success && (
-                    <AlertSuccess setSuccess={setSuccess}>{success}</AlertSuccess>
-                  )}
-                  {error && (
-                    <AlertDestructive setError={setError}>{error}</AlertDestructive>
-                  )}
-                </section>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </Card>
-      </div>
-    </>
+
+                {/* Juros */}
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="space-y-2 gap-2">
+                    <Checkbox id="jurus" />
+                    <Label htmlFor="jurus">Juros</Label>
+                  </div>
+                </div>
+
+                {/* Observações */}
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="space-y-2 gap-2">
+                    <Label htmlFor="observacoes">Obs.</Label>
+                    <Textarea className="w-full" />
+                  </div>
+                </div>
+
+              </div>
+
+              {/* Botões de Ação */}
+              <div className="flex items-end justify-end gap-2">
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={() => closeModal()}
+                >
+                  Cancelar
+                </Button>
+                <Button type="submit">Salvar</Button>
+              </div>
+            </form>
+
+            {/* Alerta de Sucesso ou Erro */}
+            <DialogFooter className="w-full">
+              <section className="w-full">
+                {success && (
+                  <AlertSuccess setSuccess={setSuccess}>{success}</AlertSuccess>
+                )}
+                {error && (
+                  <AlertDestructive setError={setError}>{error}</AlertDestructive>
+                )}
+              </section>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </Card>
+    </div>
+
   );
 
   function handleChangeData(value: any) {
